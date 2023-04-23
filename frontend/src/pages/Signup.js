55a70f0194c51c406de56720d0342969af96ccb3
Jacky Dai <ykcaj@g.ucla.edu>
2023-04-22 18:27:22 -0700

    sign up backend

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -31,93 +31,82 @@ const SignUpScreen = () => {
         alert("Please input a password. — S I D E Q U E S T");
         return;
       }
-    //   const check = await fetch(`http://localhost:5000/login`);
+      const check = await fetch(`http://localhost:4000/login`);
     
-    //   if (!check.ok) {
-    //       const message = `An error occurred: ${response.statusText}`;
-    //       window.alert(message);
-    //       return;
-    //   }
-    //   const usernamechecker = await check.json();
+      const usernamechecker = await check.json();
 
-    //   // no duplicate login
-    //   for(var login of usernamechecker) {
-    //     if(login.username == form.username) {
-    //         alert("This username has been taken. — DontBeFake");
-    //         return;
-    //     }
-    //   }
+      // no duplicate login
+      for(var login of usernamechecker) {
+        if(login.username == form.username) {
+            alert("This username has been taken. — S I D E Q U E S T");
+            return;
+        }
+      }
       
-    //   var sha1 = require('sha1');
-    //   var p1 = sha1(form.password);
-    //   var p2 = sha1(form.password2);
-    //   if(p1 !== p2) {
-    //     // TODO: make it clear they fucked up
-    //     window.location.reload(false);
-    //     return;
-    //   }
-    //   const newPerson = {username : form.username, password: p1};
+      var p1 = form.password;
+      var p2 = form.password2;
+      if(p1 !== p2) {
+        // TODO: make it clear they fucked up
+        window.location.reload(false);
+        return;
+      }
+      const newPerson = {username : form.username, password: p1};
       
-    //   await fetch("http://localhost:5000/login/add", {
-    //       method: "POST",
-    //       headers: {
-    //       "Content-Type": "application/json",
-    //       },
-    //       body: JSON.stringify(newPerson),
-    //   })
-    //   .catch(error => {
-    //       window.alert(error);
-    //       return;
-    //   });
+      await fetch("http://localhost:4000/login/add", {
+          method: "POST",
+          headers: {
+          "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newPerson),
+      })
+      .catch(error => {
+          window.alert(error);
+          return;
+      });
 
       // creates an empty profile so the database doesn't implode on itself.
-    //   const newProfileInformation = 
-    //   {
-    //     username: form.username, 
-    //     name: "",
-    //     profilePicture: 0,
-    //     userDescription: "",
-    //     joinedGroups: new Array(),
-    //   }
-    //   await fetch("http://localhost:5000/profile/add", {
-    //       method: "POST",
-    //       headers: {
-    //       "Content-Type": "application/json",
-    //       },
-    //       body: JSON.stringify(newProfileInformation),
-    //   })
-    //   .catch(error => {
-    //       window.alert(error);
-    //       return;
-    //   });
+      const newProfileInformation = 
+      {
+        username: form.username, 
+        name: "",
+        friends: new Array(),
+      }
+      await fetch("http://localhost:4000/profile/add", {
+          method: "POST",
+          headers: {
+          "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newProfileInformation),
+      })
+      .catch(error => {
+          window.alert(error);
+          return;
+      });
 
-    //   const response = await fetch(`http://localhost:5000/profile/`);
+      const response = await fetch(`http://localhost:4000/profile/`);
     
-    //   if (!response.ok) {
-    //       const message = `An error occurred: ${response.statusText}`;
-    //       window.alert(message);
-    //       return;
-    //   }
-    //   const records = await response.json();
+      if (!response.ok) {
+          const message = `An error occurred: ${response.statusText}`;
+          window.alert(message);
+          return;
+      }
+      const records = await response.json();
 
-    //   var dataBase_id = -1;
-    //   for(var record of records) {
-    //     if(record.username === form.username) {
-    //         dataBase_id = record._id;
-    //     }
-    // }
+      var dataBase_id = -1;
+      for(var record of records) {
+        if(record.username === form.username) {
+            dataBase_id = record._id;
+        }
+    }
 
-    //   localStorage.setItem("DBF_username", form.username);
-    //   localStorage.setItem("name","");
-    //   localStorage.setItem("profilePicture", 0);
-    //   localStorage.setItem("userDescription", "");
-    //   localStorage.setItem("_id", dataBase_id);
+      localStorage.setItem("SQ_username", form.username);
+      localStorage.setItem("_id", dataBase_id);
       
 
-    //   setForm({ username: "", password: "", password2: ""});
+      setForm({ username: "", password: "", password2: ""});
 
-    //   navigate("/editprofile"); //TODO: change to editprofile
-    //   window.location.reload(); // this is so navbar fixes itself
+      navigate("/"); //TODO: change to editprofile
+      window.location.reload(); // this is so navbar fixes itself
       }
 
     return (
